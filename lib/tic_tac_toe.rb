c7a3fc1483d904148a35cff8226b285f082d588d
Ashley Thompson <ashleythompsonbca@gmail.com>
2020-04-27 18:17:38 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,6 +23,13 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(user_input)
+  new_user_input= user_input.to_i
+  new_user_input -= 1
+  return new_user_input
+end
+
+
 def move(board, location, current_player = "X")
   board[location.to_i-1] = current_player
 end
